a45b9b7b99c1c87ecf4c6b431e087f5475cf46ee
Phanor Coll <phanorcoll@gmail.com>
2017-11-20 20:08:59 -0400

    base code
    initial structure of the application

---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './scss/main.scss';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
