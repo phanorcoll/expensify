9b7d0ccf3e2c7c46293f7666438590c96f347a74
Phanor Coll <phanorcoll@gmail.com>
2017-11-21 11:07:36 -0400

    Fix broken nested routes
    assets where not loading on nested routes.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
+const path = require('path')
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 var DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = merge(common, {
-    devtool: 'inline-source-map',
+    devtool: 'cheap-module-source-map',
     devServer: {
+        contentBase: path.join(__dirname, '/dist'),
         historyApiFallback: true
     },
     plugins: [
