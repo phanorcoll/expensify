ee02321006cfe002ea30fc7341fa78112d8a5c7c
Phanor Coll <phanorcoll@gmail.com>
2017-11-21 10:34:26 -0400

    broke the code into different files

---
+++ app/routers/AppRouter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage.jsx';
+import AddExpensePage from '../components/AddExpensePage.jsx';
+import EditExpensePage from '../components/EditExpensePage.jsx';
+import HelpPage from '../components/HelpPage.jsx';
+import NotFoundPage from '../components/NotFoundPage.jsx';
+import Header from '../components/Header.jsx';
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                    <Route path="/create" component={AddExpensePage} />
+                    <Route path="/edit" component={EditExpensePage} />
+                    <Route path="/help" component={HelpPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default AppRouter;
