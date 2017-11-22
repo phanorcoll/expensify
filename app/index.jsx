329c5255f6fca0b77edad722e57d2221c72beac7
Phanor Coll <phanorcoll@gmail.com>
2017-11-22 13:06:17 -0400

    setup the redux provider

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.jsx';
 import configureStore from './store/configureStore.jsx';
 import { addExpense } from './actions/expenses.jsx';
@@ -28,5 +29,10 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
