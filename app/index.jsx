b02e721a620ce6727d80087158826e7fc6330f07
Phanor Coll <phanorcoll@gmail.com>
2017-11-24 10:24:10 -0400

    ExpenseForm testing
    create the mocks for the moment package so that the component could be tested correctly

--- app/index.jsx
+++ app/index.jsx
@@ -8,6 +8,7 @@ import { setTextFilter } from './actions/filters.jsx';
 import getVisibleExpenses from './selectors/expenses.jsx';
 import 'normalize.css/normalize.css';
 import './scss/main.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
