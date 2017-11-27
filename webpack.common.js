7e4f9d57f199817cb6dde2be31c635fed47f8b5d
Phanor Coll <phanorcoll@gmail.com>
2017-11-27 12:58:26 -0400

    FIXED build issue

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         entry: './app/index.jsx'
     },
     output: {
-        path: path.resolve(__dirname, '/dist/'),
+        path: path.resolve(__dirname, 'dist/'),
         filename: 'index_bundle.js',
         publicPath: '/'
     },
